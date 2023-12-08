e897ed29b67ec7b31552a88f538cb8bdd0eb3789
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-08 20:38:13 +0100

    :arrow_up: dependencies set up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
+    id("io.realm.kotlin")
+    //id("com.google.gms.google-services")
 }
 
 android {
@@ -27,17 +31,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -48,7 +52,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -63,4 +67,55 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
+    // Firebase
+    //implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    //implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+
+    // Room components
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    // Runtime Compose
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+
+    // Splash API
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Mongo DB Realm
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("io.realm.kotlin:library-sync:1.11.0")
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    ksp("com.google.dagger:hilt-compiler:2.48")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    // Pager - Accompanist [DEPRECATED]
+//    implementation "com.google.accompanist:accompanist-pager:0.27.0"
+
+    // Date-Time Picker
+    implementation("com.maxkeppeler.sheets-compose-dialogs:core:1.0.2")
+
+    // CALENDAR
+    implementation("com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
+
+    // CLOCK
+    implementation("com.maxkeppeler.sheets-compose-dialogs:clock:1.0.2")
+
+    // Message Bar Compose
+    //implementation("com.github.stevdza-san:MessageBarCompose:1.0.5")
+
+    // One-Tap Compose
+    //implementation("com.github.stevdza-san:OneTapCompose:1.0.7")
+
+    // Desugar JDK
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
