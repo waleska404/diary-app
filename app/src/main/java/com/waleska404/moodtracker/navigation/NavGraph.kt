7a36034d9616bc6cafa33429a443543c5ed9800c
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-12-09 17:22:28 +0100

    :sparkles: authentication with google

--- app/src/main/java/com/waleska404/moodtracker/navigation/NavGraph.kt
+++ app/src/main/java/com/waleska404/moodtracker/navigation/NavGraph.kt
@@ -1,6 +1,9 @@
 package com.waleska404.moodtracker.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -10,7 +13,9 @@ import androidx.navigation.navArgument
 import com.stevdzasan.messagebar.rememberMessageBarState
 import com.stevdzasan.onetap.rememberOneTapSignInState
 import com.waleska404.moodtracker.presentation.screens.auth.AuthenticationScreen
+import com.waleska404.moodtracker.presentation.screens.auth.AuthenticationViewModel
 import com.waleska404.moodtracker.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
+import java.lang.Exception
 
 @Composable
 fun SetupNavGraph(
@@ -29,14 +34,34 @@ fun SetupNavGraph(
 
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
+        val viewModel: AuthenticationViewModel = viewModel()
+        val loadingState by viewModel.loadingState
         val oneTapState = rememberOneTapSignInState()
         val messageBarState = rememberMessageBarState()
         AuthenticationScreen(
-            loadingState = oneTapState.opened,
+            loadingState = loadingState,
             oneTapState = oneTapState,
             messageBarState = messageBarState,
             onButtonClicked = {
                 oneTapState.open()
+                viewModel.setLoading(true)
+            },
+            onTokenIdReceived = { tokenId ->
+                Log.d("MYTAG", "AUTH: $tokenId")
+                viewModel.signInWithMongoAtlas(
+                    tokenId = tokenId,
+                    onSuccess = {
+                        messageBarState.addSuccess("Authentication successful!")
+                        viewModel.setLoading(false)
+                    },
+                    onError = {
+                        messageBarState.addError(Exception(it))
+                    }
+                )
+            },
+            onDialogDismissed = { message ->
+                Log.d("MYTAG", "AUTH: $message")
+                messageBarState.addError(Exception(message))
             }
         )
     }
--- app/src/main/java/com/waleska404/moodtracker/presentation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/waleska404/moodtracker/presentation/screens/auth/AuthenticationScreen.kt
@@ -1,7 +1,6 @@
 package com.waleska404.moodtracker.presentation.screens.auth
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -10,7 +9,6 @@ import com.stevdzasan.messagebar.MessageBarState
 import com.stevdzasan.onetap.OneTapSignInState
 import com.stevdzasan.onetap.OneTapSignInWithGoogle
 import com.waleska404.moodtracker.util.Constants.CLIENT_ID
-import java.lang.Exception
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -19,7 +17,9 @@ fun AuthenticationScreen(
     loadingState: Boolean,
     oneTapState: OneTapSignInState,
     messageBarState: MessageBarState,
-    onButtonClicked: () -> Unit
+    onButtonClicked: () -> Unit,
+    onTokenIdReceived: (String) -> Unit,
+    onDialogDismissed: (String) -> Unit
 ) {
     Scaffold(
         content = {
@@ -38,12 +38,10 @@ fun AuthenticationScreen(
         state = oneTapState,
         clientId = CLIENT_ID,
         onTokenIdReceived = { tokenId ->
-            Log.d("MYTAG", "AUTH: $tokenId")
-            messageBarState.addSuccess("Authentication successful!")
+            onTokenIdReceived(tokenId)
         },
         onDialogDismissed = { message ->
-            Log.d("MYTAG", "AUTH: $message")
-            messageBarState.addError(Exception(message))
+            onDialogDismissed(message)
         }
     )
 }
